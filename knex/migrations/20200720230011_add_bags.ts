13d1be28ae7ddc20fe8376bac2ce542fbf87feb8
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-09 21:18:55 -0600

    fixed problems with the column title

--- knex/migrations/20200720230011_add_bags.ts
+++ knex/migrations/20200720230011_add_bags.ts
@@ -6,6 +6,7 @@ export const up = (knex: Knex): Promise<void> =>
     table.increments();
     table.timestamps();
     table.integer('volume');
+    table.string('title');
   });
 
 export const down = (knex: Knex): Promise<void> =>
